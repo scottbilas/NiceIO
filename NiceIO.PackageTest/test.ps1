da16fd02dd7ea438632a48e20062d7d8cc83b9bc
Scott Bilas <scottbilas@gmail.com>
2023-11-16 21:35:36 +0100

    Fixup

--- NiceIO.PackageTest/test.ps1
+++ NiceIO.PackageTest/test.ps1
@@ -1,5 +1,6 @@
+del -recur -ea:silent bin, obj, packages, ../bin, ../obj
 dotnet pack .. -p:NuspecFile=NiceIO.nuspec
-dotnet add package NiceIO --source ..\bin\release --package-directory packages
+dotnet add package OkTools.NiceIO --source ..\bin\release --package-directory packages
 if ((dotnet run) -ne 'Path is a/b/c/file.txt') { throw 'it broke' }
 ''
 'Everything is shiny'
