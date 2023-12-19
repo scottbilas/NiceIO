97889ab99f210856bef07fc811ff068e80dffacf
Scott Bilas <scottbilas@gmail.com>
2023-12-19 10:12:20 +0100

    Run from anywhere

--- NiceIO.PackageTest/test.ps1
+++ NiceIO.PackageTest/test.ps1
@@ -1,6 +1,12 @@
-del -recur -ea:silent bin, obj, packages, ../bin, ../obj
-dotnet pack .. -p:NuspecFile=NiceIO.nuspec
-dotnet add package OkTools.NiceIO --source ..\bin\release --package-directory packages
-if ((dotnet run) -ne 'Path is a/b/c/file.txt') { throw 'it broke' }
-''
-'Everything is shiny'
+pushd $PSScriptRoot
+try {
+    del -recur -ea:silent bin, obj, packages, ../bin, ../obj
+    dotnet pack .. -p:NuspecFile=NiceIO.nuspec
+    dotnet add package OkTools.NiceIO --source ..\bin\release --package-directory packages
+    if ((dotnet run) -ne 'Path is a/b/c/file.txt') { throw 'it broke' }
+    ''
+    'Everything is shiny'
+}
+finally {
+    popd
+}
