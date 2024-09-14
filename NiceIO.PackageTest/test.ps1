694ab0015a042aabc32d8dc54062c1f5f5040de4
Scott Bilas <scottbilas@gmail.com>
2024-09-14 08:23:16 +0100

    Add a little safety to the script

--- NiceIO.PackageTest/test.ps1
+++ NiceIO.PackageTest/test.ps1
@@ -1,8 +1,16 @@
 pushd $PSScriptRoot
 try {
     del -recur -ea:silent bin, obj, packages, ../bin, ../obj
+
+    dotnet restore
+    if ($LASTEXITCODE) { throw "fail to restore, error is $LASTEXITCODE" }
+
     dotnet pack .. -p:NuspecFile=NiceIO.nuspec
+    if ($LASTEXITCODE) { throw "fail to dotnet pack, error is $LASTEXITCODE" }
+
     dotnet add package OkTools.NiceIO --source ..\bin\release --package-directory packages
+    if ($LASTEXITCODE) { throw "fail to dotnet add package, error is $LASTEXITCODE" }
+
     if ((dotnet run) -ne 'Path is a/b/c/file.txt') { throw 'it broke' }
     ''
     'Everything is shiny'
